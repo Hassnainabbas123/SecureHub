da8c042be562d673d9ebc7090d78b74f3cb610c0
Hassnain abbas <abbashassnain70gmail.com>
2024-12-21 19:21:32 +0500

    Add testimonial to contactus

--- SecureHub/Realestate/src/component/Navbar.js
+++ SecureHub/Realestate/src/component/Navbar.js
@@ -4,10 +4,15 @@ import './Navbar.css';
 import logo from '../images/logo.png';
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState(false);
+  const [isContactDropdownOpen, setIsContactDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prevState) => !prevState);
+  const toggleServicesDropdown = () => {
+    setIsServicesDropdownOpen((prevState) => !prevState);
+  };
+
+  const toggleContactDropdown = () => {
+    setIsContactDropdownOpen((prevState) => !prevState);
   };
 
   return (
@@ -22,27 +27,29 @@ const Navbar = () => {
       <ul className="navbar-links">
         <li><Link to="/home">Home</Link></li>
         <li className="dropdown">
-          <a href="#services" onClick={toggleDropdown}>Services</a>
-          <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`}>
+          <a href="#services" onClick={toggleServicesDropdown}>Services</a>
+          <ul className={`dropdown-menu ${isServicesDropdownOpen ? 'show' : ''}`}>
             <li><Link to="/voting">Voting/Tokenization</Link></li>
             <li><Link to="/liquidity">Liquidity</Link></li>
             <li><Link to="/passport">Digital Passport</Link></li>
             <li><Link to="/transparency">Transparency</Link></li>
             <li><Link to="/Construction">Construction</Link></li>
             <li><Link to="/Fractional">Payment</Link></li>
-            <li><Link to="/Testimonials">Testimonials</Link></li>
           </ul>
         </li>
         <li><Link to="/aboutus">About Us</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
-      
+        <li className="dropdown">
+          <a href="/contact"onClick={toggleContactDropdown}>Contact Us</a>
+          <ul className={`dropdown-menu ${isContactDropdownOpen ? 'show' : ''}`}>
+            <li><Link to="/Testimonials">Testimonials</Link></li>
+          </ul>
+        </li>
       </ul>
 
       {/* Login and Signup Buttons */}
       <div className="navbar-buttons">
         <Link to="/login" className="login-button">Log In</Link>
         <Link to="/signup" className="signup-button">Sign Up</Link>
-
       </div>
     </nav>
   );
